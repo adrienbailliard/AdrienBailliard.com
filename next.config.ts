61e95a908112c7175a2f9d2d52d82e4edec33c16
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2025-11-27 00:33:43 +0100

    remove corp for email assets

--- next.config.ts
+++ next.config.ts
@@ -5,13 +5,18 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: "/((?!assets/email/).*)",
+        headers: [
+          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
+        ]
+      },
+      {
         source: "/(.*)",
         headers: [
           { key: "X-Frame-Options", value: "DENY" },
           { key: "X-Content-Type-Options", value: "nosniff" },
           { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
           { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Resource-Policy", value: "same-origin" },
           { key: "Content-Security-Policy", value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';" }
         ],
       }
