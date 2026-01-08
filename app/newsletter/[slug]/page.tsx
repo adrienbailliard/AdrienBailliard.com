4b8dfd8444a36c222dfeebf4fc4809c7ad01af51
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2026-01-08 15:47:11 +0100

    implement Metadata logic for newsletters but need to fix getMetadata

--- app/newsletter/[slug]/page.tsx
+++ app/newsletter/[slug]/page.tsx
@@ -1,11 +1,27 @@
 import { notFound } from "next/navigation";
+import { Metadata } from 'next';
 
 import NewsletterSignup from "@/components/newsletter/Signup";
 import NewsletterContent from "@/components/newsletter/Content";
+
+import { metadata } from "@/app/not-found";
+import { getMetadata } from "@/lib/seo/metadata";
 import { getPublishedNewsletterBySlug } from "@/lib/db/newsletters";
 
 
 export const dynamic = 'force-static';
+
+
+export async function generateMetadata({ params }: NewsletterPageProps): Promise<Metadata>
+{
+  const { slug } = await params;
+  const newsletter = await getPublishedNewsletterBySlug(slug);
+
+  return newsletter
+    ? getMetadata(`/newsletter/${slug}`)
+    : metadata;
+}
+
 
 
 type NewsletterPageProps = {
