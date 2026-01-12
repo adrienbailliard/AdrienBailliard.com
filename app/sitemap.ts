6b76cfb005c2522d8e2463252663bdc7dca2ebd3
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2026-01-12 17:04:36 +0100

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,29 @@
 import { MetadataRoute } from "next";
+
 import site from "@/config/site";
 import pages from "@/config/navigation";
-import { getPublishedNewsletterSlugs } from "@/lib/db/newsletters";
+
+import { getPublishedNewsletterPreviews } from "@/lib/db/newsletters";
+
 
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap>
 {
-  const newsletterSlugs = await getPublishedNewsletterSlugs();
-  const result: MetadataRoute.Sitemap = new Array(pages.size + newsletterSlugs.length);
+  const newsletterPreview = await getPublishedNewsletterPreviews();
+  const result: MetadataRoute.Sitemap = new Array(pages.size + newsletterPreview.length);
   let i = 0;
+
 
   for (const [key] of pages)
     result[i++] = { url: site.url + key };
 
-  for (const slug of newsletterSlugs)
-    result[i++] = { url: `${site.url}/newsletter/${slug}` };
+
+  for (const newsletter of newsletterPreview)
+    result[i++] = {
+      url: `${site.url}/newsletter/${newsletter.slug}`,
+      lastModified: newsletter.updated_at
+    };
+
 
   return result;
 }
