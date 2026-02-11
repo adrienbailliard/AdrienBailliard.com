f3b031af379206ead568a16f0b89ffedcf7faaae
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2026-02-12 00:37:20 +0100

    rewrite consultation page text

--- app/consultation/page.tsx
+++ app/consultation/page.tsx
@@ -15,7 +15,7 @@ import { StaticPage } from "@/lib/types";
 const page: StaticPage = {
   pathname: "/consultation",
   title: "Consultation",
-  description: "Réserve une consultation 1:1 avec Adrien Bailliard pour automatiser ton business avec des systèmes performants, comme les meilleurs dirigeants."
+  description: "Réserve une consultation 1:1 avec Adrien Bailliard pour bâtir les systèmes qui automatiseront ta croissance."
 };
 
 
@@ -38,11 +38,11 @@ export default function Consultation()
           </h1>
           <div className="max-lg:flex max-lg:justify-between mt-9 sm:mt-12 md:mt-16 lg:mt-9 max-lg:items-center">
             <p className="max-w-2xs sm:max-w-xs md:max-w-sm lg:w-[50%] lg:max-w-2xl">
-              Réserve ta consultation 1:1 pour créer des systèmes sur mesure, conçus pour libérer ton temps.
+              Réserve ta consultation 1:1 pour bâtir des systèmes qui stabiliseront ta croissance et libéreront ton temps.
             </p>
             <Image
               src="/assets/illustrations/consultation.png"
-              alt="Consultation 1:1"
+              alt="Consultation 1:1 - Adrien Bailliard"
               width={875}
               height={500}
               className="w-48 sm:w-sm md:w-md lg:absolute max-sm:-mr-16 max-lg:-mr-30 lg:-right-35 lg:top-1/2 lg:-translate-y-1/2 lg:w-2xl xl:w-3xl 2xl:w-4xl"
@@ -54,7 +54,7 @@ export default function Consultation()
               href="#cta"
               className="group base-link text-light-muted-fg mt-8 md:mt-10 inline-flex"
             >
-              {"Découvre comment"}
+              {"Découvre la méthode"}
               <Arrow className="base-arrow rotate-90 shrink-0 group-hover:translate-y-0.5" />
             </a>
           </div>
@@ -62,8 +62,8 @@ export default function Consultation()
       </section>
 
       <Cta
-        title="Parle-Moi de ton Projet"
-        paragraph="Chaque système est conçu pour ton activité. Voyons comment libérer ton temps et affiner tes process."
+        title="Bâtissons tes Systèmes"
+        paragraph="Chaque architecture est pensée pour ton activité. Analysons tes process pour libérer ton temps."
         variant="light"
         className="-mt-16 md:-mt-26 lg:-mt-30"
       >
@@ -72,7 +72,7 @@ export default function Consultation()
           href="/contact"
           variant="light-primary"
         >
-          { "Réserve mon Audit Gratuit" }
+          { "Réserve mon Diagnostic" }
         </Link>
       </Cta>
     </main>
