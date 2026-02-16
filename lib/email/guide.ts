d6b085d6bf65a045b8b912d8a7b6e79157a987c8
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2026-02-17 00:44:58 +0100

    optimise static email templates performance

--- lib/email/guide.ts
+++ lib/email/guide.ts
@@ -6,51 +6,52 @@ import { stylizeBodyContent } from "@/lib/email/stylizer";
 
 
 
+const staticHtml = layout(`
+  <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="background-color: #0F0F0F; text-align: center; width: 100%;
+    background-image: url('${site.url}${site.illustrationsFolder}le-systeme-d-elite.png'); background-repeat: no-repeat; background-position: center bottom; background-size: 224px 317px; height: 381px;"
+  >
+    <tr><td></td></tr>
+    <tr style="background-image: url('${site.url}${site.emailAssetsFolder}guide-gradient.png'); background-size: 660px 68px;">
+      <td height="68px" style="font-size: 32px; padding-left: 5%; padding-right: 5%; vertical-align: bottom; color: white;">
+        Les Secrets du Top 1%
+      </td>
+    </tr>
+    <tr style="background: black;">
+      <td height="64px"></td>
+    </tr>
+  </table>
+
+
+  ${ stylizeBodyContent(`
+    <div style="background-color: #F0F0F0; padding-left: 5%; padding-right: 5%; padding-top: 52px; padding-bottom: 64px;">
+      <p>
+        Hello,
+      </p>
+      <p>
+        Comme convenu, voici ton accès au guide :
+        <br>
+        → <a href="${site.url}${site.documentsFolder}Le%20Syst%C3%A8me%20d'%C3%89lite%20-%20Adrien%20Bailliard.pdf">Télécharger le PDF</a>
+      </p>
+      <p>
+        Découvre comment automatiser au-delà de la <em>performance</em>.
+      </p>
+      <p>_</p>
+      <p>
+        ${ site.name }
+      </p>
+    </div>
+  `) }
+`);
+
+
+
 export async function sendGuide(email: string): Promise<void>
 {
-    const content = `
-      <table border="0" cellpadding="0" cellspacing="0" role="presentation" style="background-color: #0F0F0F; text-align: center; width: 100%;
-        background-image: url('${site.url}${site.illustrationsFolder}le-systeme-d-elite.png'); background-repeat: no-repeat; background-position: center bottom; background-size: 224px 317px; height: 381px;"
-      >
-        <tr><td></td></tr>
-        <tr style="background-image: url('${site.url}${site.emailAssetsFolder}guide-gradient.png'); background-size: 660px 68px;">
-          <td height="68px" style="font-size: 32px; padding-left: 5%; padding-right: 5%; vertical-align: bottom; color: white;">
-            Les Secrets du Top 1%
-          </td>
-        </tr>
-        <tr style="background: black;">
-          <td height="64px"></td>
-        </tr>
-      </table>
-
-
-      ${ stylizeBodyContent(`
-        <div style="background-color: #F0F0F0; padding-left: 5%; padding-right: 5%; padding-top: 52px; padding-bottom: 64px;">
-          <p>
-            Hello,
-          </p>
-          <p>
-            Comme convenu, voici ton accès au guide :
-            <br>
-            → <a href="${site.url}${site.documentsFolder}Le%20Syst%C3%A8me%20d'%C3%89lite%20-%20Adrien%20Bailliard.pdf">Télécharger le PDF</a>
-          </p>
-          <p>
-            Découvre comment automatiser au-delà de la <em>performance</em>.
-          </p>
-          <p>_</p>
-          <p>
-            ${ site.name }
-          </p>
-        </div>
-      `) }
-    `;
-
-
     await resend.emails.send({
         from: process.env.EMAIL_SENDER!,
         to: [email],
         replyTo: process.env.EMAIL_RECEIVER!,
         subject: "[Le Système d'Élite] Ton Guide Est Prêt",
-        html: layout(content)
+        html: staticHtml
     });
 }
--- lib/email/newsletter.ts
+++ lib/email/newsletter.ts
@@ -16,61 +16,62 @@ import { generateJWT } from "@/lib/security";
 
 
 
+const staticHtml = layout(stylizeBodyContent(`
+    <div style="background-color: #F0F0F0; padding-left: 5%; padding-right: 5%; padding-top: 36px; padding-bottom: 64px;">
+        <p>
+            Hello,
+        </p>
+        <p>
+            Il est temps de franchir un cap et de libérer ton temps grâce à l'automatisation.
+        </p>
+        <p>
+            Pour cela, tu dois comprendre les clés des systèmes du Top 1%.
+        </p>
+        <h3>
+            LE GAIN
+        </h3>
+        <p>
+            Tous les lundis, un système pour automatiser <em>intelligemment</em>.
+        </p>
+        <p>
+            Je ne partage que ce qui a une réelle valeur.
+        </p>
+        <h3>
+            LA PRÉPARATION
+        </h3>
+        <p>
+            Pour être sûr de recevoir mes emails :
+        </p>
+        <ul>
+            <li>
+                Réponds « merci » à ce mail.
+            </li>
+            <li>
+                Si ce mail a atterri dans ton dossier Spam ou Promotions, déplace-le dans ta boîte de réception principale.
+            </li>
+        </ul>
+        <p>_</p>
+        <p>
+            À très vite !
+            <br />
+            ${ site.name }
+        </p>
+        <p>
+            PS : En attendant la prochaine newsletter, tu peux <a href="${site.url}/consultation">réserver une consultation 1:1</a> pour libérer ton temps grâce à des systèmes sur mesure.
+        </p>
+    </div>
+`), true);
+
+
+
 export async function sendConfirmation(email: string): Promise<void>
 {
-    const content = stylizeBodyContent(`
-        <div style="background-color: #F0F0F0; padding-left: 5%; padding-right: 5%; padding-top: 36px; padding-bottom: 64px;">
-            <p>
-                Hello,
-            </p>
-            <p>
-                Il est temps de franchir un cap et de libérer ton temps grâce à l'automatisation.
-            </p>
-            <p>
-                Pour cela, tu dois comprendre les clés des systèmes du Top 1%.
-            </p>
-            <h3>
-                LE GAIN
-            </h3>
-            <p>
-                Tous les lundis, un système pour automatiser <em>intelligemment</em>.
-            </p>
-            <p>
-                Je ne partage que ce qui a une réelle valeur.
-            </p>
-            <h3>
-                LA PRÉPARATION
-            </h3>
-            <p>
-                Pour être sûr de recevoir mes emails :
-            </p>
-            <ul>
-                <li>
-                    Réponds « merci » à ce mail.
-                </li>
-                <li>
-                    Si ce mail a atterri dans ton dossier Spam ou Promotions, déplace-le dans ta boîte de réception principale.
-                </li>
-            </ul>
-            <p>_</p>
-            <p>
-                À très vite !
-                <br />
-                ${ site.name }
-            </p>
-            <p>
-                PS : En attendant la prochaine newsletter, tu peux <a href="${site.url}/consultation">réserver une consultation 1:1</a> pour libérer ton temps grâce à des systèmes sur mesure.
-            </p>
-        </div>
-    `);
-
-
     await resend.emails.send({
         from: process.env.EMAIL_SENDER!,
         to: [email],
         replyTo: process.env.EMAIL_RECEIVER!,
         subject: "Bienvenue dans Auto Monday",
-        html: layout(content, true)
+        html: staticHtml
     });
 }
 
