c200c4d73cc94043b9152d4d0ceabdaa8713776e
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2026-02-20 16:26:31 +0100

    improve not found page copywriting

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import { getUtilityMetadata } from "@/lib/seo/metadata";
 
 
 
-export const metadata = getUtilityMetadata("Destination Inconnue");
+export const metadata = getUtilityMetadata("Hors Périmètre");
 
 
 export default function NotFound()
@@ -11,7 +11,7 @@ export default function NotFound()
   return (
     <main className="bg-dark-bg">
       <section className="hero text-center">
-        <h2>Cette Destination est Inconnue</h2>
+        <h2>Cette Page est Hors Périmètre</h2>
         <Link href="/" variant="primary">
           { "Retour à la Base" }
         </Link>
