a516fdcfc5241f75bc84f77bfb823f922988858b
Adrien Bailliard <187867341+adrienbailliard@users.noreply.github.com>
2026-03-12 16:02:30 +0100

    update linkedin stat

--- app/a-propos/page.tsx
+++ app/a-propos/page.tsx
@@ -50,7 +50,7 @@ export default function About()
               70&nbsp;000€ générés
             </li>
             <li>
-              280&nbsp;000 impressions
+              320&nbsp;000 impressions
             </li>
             <li>
               { "Systèmes critiques" }
